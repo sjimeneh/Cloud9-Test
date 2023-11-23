703e59faf1ec8c36471c8c11971668a1d35de132
EC2 Default User <ec2-user@ip-172-31-18-168.ec2.internal>
2023-11-23 05:41:36 +0000

    function lambda improve

--- test-api-lambda/lambda.py
+++ test-api-lambda/lambda.py
@@ -10,7 +10,14 @@ def read_csv(file):
             print(row)
             
 def lambda_handler(event, context):
-    #read_csv('data')
+    print(event)
+    
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    objeto = event['Records'][0]['s3']['bucket']['key']
+    
+    print(f'Bucket {bucket}')
+    print(f'Objeto {objeto}')
+    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
